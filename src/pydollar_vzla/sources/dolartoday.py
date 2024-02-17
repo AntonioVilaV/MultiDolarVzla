cc69d4775c7bfaff9d2fc8a6f9d9464f0fb1041b
Antonio Vila <vila.antoniojose@gmail.com>
2024-02-17 19:36:37 -0400

    use of playwright to make a request to dolartoday

--- src/pydollar_vzla/sources/dolartoday.py
+++ src/pydollar_vzla/sources/dolartoday.py
@@ -1,12 +1,7 @@
 import re
 
-from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
-from pydollar_vzla.config.driver_config import DriverConfig
 
 from .base import DolarSource
 from .urls import URL_DOLARTODAY
@@ -17,16 +12,14 @@ class DolarTodayExtractor(DolarSource):
     Class to extract parallel dollar data from the DolarToday website.
     """
 
-    def __init__(self, driver_type: str):
+    def __init__(self):
         """
         Initializes the DolarTodayExtractor instance with the specified driver type.
 
         Args:
             driver_type (str): Type of driver to use for web scraping.
         """
-        super().__init__(driver_type)
         self._url = URL_DOLARTODAY
-        self._driver = DriverConfig(driver_type)
 
     def get_dolar_data(self):
         """
@@ -35,48 +28,45 @@ class DolarTodayExtractor(DolarSource):
         Returns:
             str: Raw data of the parallel dollar.
         """
+
         try:
-            driver = self._driver.get_driver()
-            driver.get(self._url)
+            with sync_playwright() as p:
+                browser = p.chromium.launch()
+                page = browser.new_page()
+                page.goto(self._url)
 
-            amount_input = WebDriverWait(driver, 10).until(
-                EC.visibility_of_element_located((By.ID, "amount"))
-            )
+                page.wait_for_selector("#amount")
+                amount_input = page.locator("#amount")
+                amount_input.clear()
+                amount_input.fill("1")
 
-            amount_input.clear()
-            amount_input.send_keys("1")
+                page.wait_for_selector("#calculate-button")
+                calculate_button = page.locator("#calculate-button")
+                calculate_button.click()
 
-            calculate_button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.ID, "calculate-button"))
-            )
+                wait_function_script = """
+                    function() {
+                        const resultText = document.querySelector('#dt-currency-calculator-results p:nth-of-type(1)').textContent;
+                        return resultText !== 'Dólar Paralelo: Bs. 0';
+                    }
+                """
+                page.wait_for_function(wait_function_script)
 
-            calculate_button.click()
-
-            WebDriverWait(driver, 10).until(
-                lambda driver: not EC.text_to_be_present_in_element(
-                    (By.CSS_SELECTOR, "#dt-currency-calculator-results p"), "Bs. 0"
-                )(driver)
-            )
-
-            html_content = driver.page_source
-
-            driver.quit()
-            soup = BeautifulSoup(html_content, "html.parser")
-            results_div = soup.find("div", id="dt-currency-calculator-results")
-
-            parallel_dollar_span = results_div.find("span", text="Dólar Paralelo")
-
-            price = "0:0"
-            if parallel_dollar_span:
-                parent_element = parallel_dollar_span.parent
-                parallel_dollar_text = parent_element.get_text(strip=True)
-                price = parallel_dollar_text.split(":")[1]
-                price = price.split("Bs.")[1]
-            return price
+                parallel_dollar_element = page.locator(
+                    "#dt-currency-calculator-results p:nth-of-type(1)"
+                )
+                parallel_dollar_value = parallel_dollar_element.inner_text()
 
         except TimeoutException:
             print("Timeout when waiting for an element on the page.")
             return None
+
+        try:
+            price = "0.0"
+            if parallel_dollar_value:
+                price = parallel_dollar_value.split(":")[1]
+                price = price.split("Bs.")[1]
+            return price
 
         except Exception as e:
             print(f"An unexpected error occurred: {str(e)}")
@@ -89,10 +79,13 @@ class DolarTodayExtractor(DolarSource):
         Returns:
             float: Cleaned price of the parallel dollar.
         """
-        price = self.get_dolar_data() if self.get_dolar_data() else "0:0"
+        price = self.get_dolar_data()
+        if price is None:
+            return 0
         cleaned_price_str = re.sub(r"[^\d.,]", "", price)
         cleaned_price_str = cleaned_price_str.replace(",", ".")
         cleaned_price = float(cleaned_price_str)
+        cleaned_price = round(cleaned_price, 2)
         return cleaned_price
 
     def get_dolar_price(self) -> float:
